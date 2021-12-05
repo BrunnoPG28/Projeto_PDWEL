d367edde98d23b26c9a5e07e5dbffdac4f441d4c
BRUNNO PAPIS GUSMÃO <brunnopg28@hotmail.com>
2021-12-05 19:19:35 -0300

    Script sql para criação do banco

---
+++ BD/script.sql
@@ -0,0 +1,25 @@
+--CREATE DATABASE agenda;
+--use agenda;
+
+CREATE TABLE `contatos` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nome` varchar(100) NOT NULL DEFAULT '0',
+  `descricao` varchar(100) NOT NULL DEFAULT '0',
+  `telefone` varchar(50) NOT NULL DEFAULT '0',
+  `email` varchar(50) NULL,
+  `endereço` varchar(50) NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- Índices para tabela `tbagenda`
+--
+ALTER TABLE `contatos`
+  ADD KEY `Index 1` (`id`);
+
+--
+-- AUTO_INCREMENT de tabela `tbagenda`
+--
+ALTER TABLE `contatos`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=177;
+  
+--select * from contatos;
