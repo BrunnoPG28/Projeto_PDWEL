33a6448fbc9f4fc6f30add88d2c6d30ff76fe16a
Dimas de Oliveira <dimasdeoliveira01@gmail.com>
2021-12-08 20:21:36 -0300

    adição dos controllers

---
+++ controllers/agendaController.php
@@ -0,0 +1,79 @@
+<?php
+class agendaController extends controller
+{
+	public function __construct()
+	{
+		parent ::__construct();
+	}
+
+
+	public function index()
+	{
+			$dados = array
+			(
+				'agenda' => array()
+		 	);
+
+			$ag = new Agenda();
+			$dados['agenda'] = $ag->getLista();
+
+			$this->loadTemplate('agenda', $dados);
+
+	}
+
+	public function cadastrar()
+	{
+			$dados = array();
+
+			if(isset($_POST['nome'])&&(!empty($_POST['nome'])))
+			{
+				$nome = strtoupper (addslashes($_POST['nome']) );
+				$desc = addslashes($_POST['descricao']);
+				$tel = addslashes($_POST['telefone']);
+
+				$a = new Agenda();
+				$a->cad($nome, $desc, $tel);
+
+				echo "<meta http-equiv='refresh' content=0;url=". BASE_URL . "agenda>";    	
+			}
+
+			$this->loadTemplate('cadastrar', $dados);
+	}
+
+	public function editar($id=0)
+	{
+			$dados = array(
+				'agenda' => array()
+
+			     );
+
+			$e = new Agenda();
+
+			$dados['agenda'] = $e->getDados($id);
+
+			if(isset($_POST['nome'])&&(!empty($_POST['nome'])))
+			{
+				$id = addslashes($id);
+
+				$nome = strtoupper (addslashes($_POST['nome']) );
+				$desc = addslashes($_POST['descricao']);
+				$tel = addslashes($_POST['telefone']);
+
+				$e->editar($nome, $desc, $tel, $id);
+
+				echo "<meta http-equiv='refresh' content=0;url=". BASE_URL . "agenda>";    	
+			}
+			$this->loadTemplate('agenda_editar', $dados);
+
+	}
+
+    public function excluir($id)
+	{
+			$e = new Agenda();
+			$id = addslashes($id);
+			$e->excluir($id);
+
+			echo "<meta http-equiv='refresh' content=0;url=". BASE_URL . "agenda>";    	
+	}
+}
+?>
---
+++ controllers/homeController.php
@@ -0,0 +1,15 @@
+<?php
+
+class homeController extends controller{
+
+	public function index(){
+
+        $dados = array();
+
+		    echo "<meta http-equiv='refresh' content=0;url=". BASE_URL . "agenda>";    	
+
+	}
+
+
+	
+}
